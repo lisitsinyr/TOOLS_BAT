e48973776f62b2ca796de4fee224ba2d9112d995
lisitsinyr <lisitsinyr@gmail.com>
2024-02-11 15:05:57 +0400

    Git Bash commit update

--- BAT/[lyrxxx_]ШАБЛОН.bat
+++ BAT/[lyrxxx_]ШАБЛОН.bat
@@ -1,13 +1,14 @@
 @echo off
 rem -------------------------------------------------------------------
 rem lyrxxx_ШАБЛОН.bat
+rem    LOG_DIR=
 rem ----------------------------------------------------------------------------
 chcp 1251
 
-rem ====================================================================
-rem 1.PROJECTS
-rem -------------------------------------------------------------------
-set PROJECTS=PROJECTS_KIX
+echo =======================================================
+echo 1.PROJECTS
+echo -------------------------------------------------------
+set PROJECTS=PROJECTS_BAT
 echo PROJECTS: %PROJECTS%
 set CURRENT_SYSTEM=%OS%
 echo CURRENT_SYSTEM: %CURRENT_SYSTEM%
@@ -17,21 +18,38 @@ set USERNAME=%USERNAME%
 echo USERNAME: %USERNAME%
 set PROJECTS_LYR_DIR=D:\PROJECTS_LYR
 echo PROJECTS_LYR_DIR: %PROJECTS_LYR_DIR%
-set PROJECTS_DIR=%PROJECTS_LYR_DIR%\CHECK_LIST03_SCRIPT\01_KIX\%PROJECTS%
+set PROJECTS_DIR=%PROJECTS_LYR_DIR%\CHECK_LIST\03_SCRIPT\04_BAT\%PROJECTS%
 echo PROJECTS_DIR: %PROJECTS_DIR%
-rem  Файл скрипта: каталог+имя+расширение
-set SCRIPT_FULLNAME=%~f0
-echo SCRIPT_FULLNAME: %SCRIPT_FULLNAME%
-rem  Файл скрипта: имя+расширение
-set SCRIPT_BASENAME=%~n0%~x0
-echo SCRIPT_BASENAME: %SCRIPT_BASENAME%
-rem  Файл скрипта: имя
+rem Файл скрипта: каталог+имя+расширение
+set SCRIPT_FULLFILENAME=%~f0
+echo SCRIPT_FULLFILENAME: %SCRIPT_FULLFILENAME%
+rem Файл скрипта: имя+расширение
+set SCRIPT_BASEFILENAME=%~n0%~x0
+echo SCRIPT_BASEFILENAME: %SCRIPT_BASEFILENAME%
+rem Файл скрипта: имя
 set SCRIPT_FILENAME=%~n0
 echo SCRIPT_FILENAME: %SCRIPT_FILENAME%
 
-rem ====================================================================
-rem 2.БИБЛИОТЕКА LYR
-rem -------------------------------------------------------------------
+echo =======================================================
+echo 2.Каталог журналов
+echo -------------------------------------------------------
+set LOG_DIR=%~d0%~p0LOGS
+echo LOG_DIR: %LOG_DIR%
+set LOG_DIR=%PROJECTS_LYR_DIR%\CHECK_LIST\03_SCRIPT\04_BAT\%PROJECTS%\LOGS
+echo LOG_DIR: %LOG_DIR%
+rem Файл журнала: имя+расширение
+set LOG_BASEFILENAME=""
+echo LOG_BASEFILENAME: %LOG_BASEFILENAME%
+rem Файл журнала: каталог+имя+расширение
+set LOG_FULLFILENAME=%~d0%~p0LOGS\%~n0%~x0.log
+echo LOG_FULLFILENAME: %LOG_FULLFILENAME%
+rem Параметры журнала
+set LOG_OPT=11
+echo LOG_OPT: %LOG_OPT%
+
+echo =======================================================
+echo 3.БИБЛИОТЕКА LYR
+echo -------------------------------------------------------
 if "%LIB_KIX%" == "" goto SetKXLDIR
 goto OK
 :SetKXLDIR
@@ -46,22 +64,9 @@ set LIB_KIX=D:\TOOLS\TOOLS_BAT\TOOLS_KIX
 :OK
 echo LIB_KIX: %LIB_KIX%
 
-rem =======================================================
-rem 3.Каталог журналов
-rem -------------------------------------------------------------------
-set LOG_DIR=%~d0%~p0LOGS
-echo LOG_DIR: %LOG_DIR%
-rem  Файл журнала: каталог+имя+расширение
-set LOG_FILE=%~d0%~p0LOGS\%~n0%~x0.log
-echo LOG_FILE: %LOG_FILE%
-rem  Параметры журнала
-set LOG_OPT=11
-echo LOG_OPT: %LOG_OPT%
-rem =======================================================
-
-rem =======================================================
-rem 4.SCRIPT_KIX
-rem -------------------------------------------------------------------
+echo =======================================================
+echo 4.SCRIPT_KIX
+echo -------------------------------------------------------
 set KIX_DIR=""
 
 set SCRIPT_KIX=lyrxxx_ШАБЛОН.kix
