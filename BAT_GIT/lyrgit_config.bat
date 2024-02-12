b482edfc0d22844444f8f7016ab534d2a23a1998
lisitsinyr <lisitsinyr@gmail.com>
2024-02-12 12:17:09 +0400

    Git Bash commit update

--- BAT_GIT/lyrgit_config.bat
+++ BAT_GIT/lyrgit_config.bat
@@ -54,41 +54,54 @@ rem ----------------------------------------------------------------------------
 chcp 1251
 
 rem -------------------------------------------------------------------
-rem РљР°С‚Р°Р»РѕРі Р¶СѓСЂРЅР°Р»Р°: РєР°С‚Р°Р»РѕРі
+rem Каталог журнала: каталог
 rem     LOG_DIR=
-rem Р¤Р°Р№Р» Р¶СѓСЂРЅР°Р»Р°: РёРјСЏ+СЂР°СЃС€РёСЂРµРЅРёРµ
+rem Файл журнала: имя+расширение
 rem     LOG_BASEFILENAME=
-rem Р¤Р°Р№Р» Р¶СѓСЂРЅР°Р»Р°: РёРјСЏ
+rem Файл журнала: имя
 rem     LOG_FILENAME=
-rem Р¤РѕСЂРјР°С‚ РёРјРµРЅРё С„Р°Р№Р»С„ Р¶СѓСЂРЅР°Р»Р°: FILENAME,DT,...
+rem Формат имени файлф журнала: FILENAME,DT,...
 rem     LOG_FILENAME_FORMAT=
-rem РџР°СЂР°РјРµС‚СЂС‹ Р¶СѓСЂРЅР°Р»Р°
+rem Параметры журнала
 rem     LOG_OPT=11
 rem -------------------------------------------------------------------
 
 rem echo -------------------------------------------------------
 rem echo 0.DEFAULT
 rem echo -------------------------------------------------------
+set REPO=REPO.ini
+rem Проверка существования файла REPO
+if not exist %REPO% (
+    echo ERROR: Файл %REPO% с настройками отсутствует
+    exit /b 1
+)
+for /f "eol=# delims== tokens=1,2" %%i in (%REPO%) do (
+   rem В переменной i - ключ
+   rem В переменной j - значение
+   set %%i=%%j
+)
+rem echo REPO_NAME: %REPO_NAME%
 
 set LOG_DIR=.
 set LOG_DIR=D:\PROJECTS_LYR\LOGS
-set LOG_FILENAME=GIT_CONFIG_list_so_ss_current
+set LOG_FILENAME=%REPO_NAME%_CONFIG_list_so_ss_current
+rem echo LOG_FILENAME: %LOG_FILENAME%
 
-rem ...РљР°С‚Р°Р»РѕРі BAT_DIR: РєР°С‚Р°Р»РѕРі
+rem ...Каталог BAT_DIR: каталог
 if "%BAT_DIR%" == "" (
     set BAT_DIR=D:\TOOLS\TOOLS_BAT\BAT
 )
-echo BAT_DIR: %BAT_DIR%
+rem echo BAT_DIR: %BAT_DIR%
 rem ...DATETIME_STAMP
 set DATETIME_STAMP=%date:~6,4%%date:~3,2%%date:~0,2%%TIME:~0,2%%TIME:~3,2%%TIME:~6,2%
 rem echo DATETIME_STAMP: %DATETIME_STAMP%
-rem ...Р¤РѕСЂРјР°С‚ РёРјРµРЅРё С„Р°Р№Р»С„ Р¶СѓСЂРЅР°Р»Р°: FILENAME,DT,...
+rem ...Формат имени файлф журнала: FILENAME,DT,...
 if "%LOG_FILENAME_FORMAT%" == "" (
     set LOG_FILENAME_FORMAT=FILENAME
     rem set LOG_FILENAME_FORMAT=DATETIME
 )
 rem echo LOG_FILENAME_FORMAT: %LOG_FILENAME_FORMAT%
-rem ...РџР°СЂР°РјРµС‚СЂС‹ Р¶СѓСЂРЅР°Р»Р°
+rem ...Параметры журнала
 if "%LOG_OPT%" == "" (
     set LOG_OPT=11
 )
@@ -108,31 +121,31 @@ set PROJECTS_LYR_DIR=D:\PROJECTS_LYR
 rem echo PROJECTS_LYR_DIR: %PROJECTS_LYR_DIR%
 set PROJECTS_DIR=%PROJECTS_LYR_DIR%\CHECK_LIST\03_SCRIPT\04_BAT\%PROJECTS%
 rem echo PROJECTS_DIR: %PROJECTS_DIR%
-rem ...Р¤Р°Р№Р» СЃРєСЂРёРїС‚Р°: РєР°С‚Р°Р»РѕРі+РёРјСЏ+СЂР°СЃС€РёСЂРµРЅРёРµ
+rem ...Файл скрипта: каталог+имя+расширение
 set SCRIPT_FULLFILENAME=%~f0
 rem echo SCRIPT_FULLFILENAME: %SCRIPT_FULLFILENAME%
-rem ...Р¤Р°Р№Р» СЃРєСЂРёРїС‚Р°: РёРјСЏ+СЂР°СЃС€РёСЂРµРЅРёРµ
+rem ...Файл скрипта: имя+расширение
 set SCRIPT_BASEFILENAME=%~n0%~x0
 rem echo SCRIPT_BASEFILENAME: %SCRIPT_BASEFILENAME%
-rem ...Р¤Р°Р№Р» СЃРєСЂРёРїС‚Р°: РёРјСЏ
+rem ...Файл скрипта: имя
 set SCRIPT_FILENAME=%~n0
 rem echo SCRIPT_FILENAME: %SCRIPT_FILENAME%
 rem echo -------------------------------------------------------
-rem echo 2.РљР°С‚Р°Р»РѕРі Р¶СѓСЂРЅР°Р»РѕРІ
+rem echo 2.Каталог журналов
 rem echo -------------------------------------------------------
-rem РљР°С‚Р°Р»РѕРі Р¶СѓСЂРЅР°Р»Р°: РєР°С‚Р°Р»РѕРі
+rem Каталог журнала: каталог
 if "%LOG_DIR%" == "" (
     rem set LOG_FULLFILENAME=%~d0%~p0LOGS
     set LOG_DIR=%PROJECTS_LYR_DIR%\CHECK_LIST\03_SCRIPT\04_BAT\%PROJECTS%\LOGS
 )
 rem echo LOG_DIR: %LOG_DIR%
 if exist %LOG_DIR% (
-    rem echo РљР°С‚Р°Р»РѕРі %LOG_DIR% СЃСѓС‰РµСЃС‚РІСѓРµС‚
+    rem echo Каталог %LOG_DIR% существует
 ) else (
-    echo ERROR: РљР°С‚Р°Р»РѕРі %LOG_DIR% РЅРµ СЃСѓС‰РµСЃС‚РІСѓРµС‚
+    echo ERROR: Каталог %LOG_DIR% не существует
     exit /b 1
 )
-rem ...Р¤Р°Р№Р» Р¶СѓСЂРЅР°Р»Р°: РёРјСЏ
+rem ...Файл журнала: имя
 if "%LOG_FILENAME%" == "" (
     if "%LOG_FILENAME_FORMAT%" == "FILENAME" (
         set LOG_FILENAME=%SCRIPT_FILENAME%
@@ -140,22 +153,22 @@ if "%LOG_FILENAME%" == "" (
         if "%LOG_FILENAME_FORMAT%" == "DATETIME" (
             set LOG_FILENAME=%DATETIME_STAMP%
         ) else (
-            echo ERROR: Р¤Р°Р№Р» Р¶СѓСЂРЅР°Р»Р° РЅРµ Р·Р°РґР°РЅ
+            echo ERROR: Файл журнала не задан
             exit /b 1
         )
     )
 )
 rem echo LOG_FILENAME: %LOG_FILENAME%
-rem ...Р¤Р°Р№Р» Р¶СѓСЂРЅР°Р»Р°: РєР°С‚Р°Р»РѕРі+РёРјСЏ+СЂР°СЃС€РёСЂРµРЅРёРµ
+rem ...Файл журнала: каталог+имя+расширение
 set LOG_FULLFILENAME=%LOG_DIR%\%LOG_FILENAME%.log
 rem echo LOG_FULLFILENAME: %LOG_FULLFILENAME%
-rem ...РџР°СЂР°РјРµС‚СЂС‹ Р¶СѓСЂРЅР°Р»Р° LOG_OPT1
+rem ...Параметры журнала LOG_OPT1
 set LOG_OPT1=%LOG_OPT:~0,1%
 if "%LOG_OPT1%" == "" (
     set LOG_OPT1=1
 )
 rem echo LOG_OPT1: %LOG_OPT1%
-rem ...РџР°СЂР°РјРµС‚СЂС‹ Р¶СѓСЂРЅР°Р»Р° LOG_OPT2
+rem ...Параметры журнала LOG_OPT2
 set LOG_OPT2=%LOG_OPT:~1,1%
 if "%LOG_OPT2%" == "" (
     set LOG_OPT2=1
@@ -163,7 +176,7 @@ if "%LOG_OPT2%" == "" (
 rem echo LOG_OPT2: %LOG_OPT2%
 
 echo -------------------------------------------------------
-echo 5. Р—Р°РїСѓСЃРє
+echo 5. Запуск
 echo -------------------------------------------------------
 
 :begin
