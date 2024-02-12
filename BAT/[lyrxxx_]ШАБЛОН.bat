376aa972fe3add0ec57c3641a1abc690b5f1dbba
lisitsinyr <lisitsinyr@gmail.com>
2024-02-12 15:11:29 +0400

    Git Bash commit update

--- BAT/[lyrxxx_]ШАБЛОН.bat
+++ BAT/[lyrxxx_]ШАБЛОН.bat
@@ -4,16 +4,11 @@ rem [lyrxxx_]ШАБЛОН.bat
 rem -------------------------------------------------------------------
 chcp 1251
 
+rem -------------------------------------------------------------------
 rem Каталог журнала: каталог
 rem     LOG_DIR=
 rem Файл журнала: имя+расширение
 rem     LOG_BASEFILENAME=
-rem Каталог библиотеки KIX: каталог
-rem     LIB_KIX=
-rem Каталог APP_KIX: каталог
-rem     KIX_DIR=
-rem Скрипт APP_KIX: имя+расширение
-rem     APP_KIX=lyrxxx_ШАБЛОН.kix
 rem Файл журнала: имя
 rem     LOG_FILENAME=
 rem Формат имени файлф журнала: FILENAME,DT,...
@@ -25,18 +20,26 @@ rem -------------------------------------------------------------------
 rem echo -------------------------------------------------------
 rem echo 0.DEFAULT
 rem echo -------------------------------------------------------
+set REPO_NAME=
 set REPO=REPO.ini
 rem Проверка существования файла REPO
 if not exist %REPO% (
-    echo ERROR: Файл %REPO% с настройками отсутствует
-    exit /b 1
-)
-for /f "eol=# delims== tokens=1,2" %%i in (%REPO%) do (
-   rem В переменной i - ключ
-   rem В переменной j - значение
-   set %%i=%%j
+    echo Файл %REPO% с настройками отсутствует
+    rem exit /b 1
+) else (
+    for /f "eol=# delims== tokens=1,2" %%i in (%REPO%) do (
+       rem В переменной i - ключ
+       rem В переменной j - значение
+       set %%i=%%j
+    )
 )
 rem echo REPO_NAME: %REPO_NAME%
+
+set LOG_DIR=.
+set LOG_DIR=D:\PROJECTS_LYR\LOGS
+set LOG_FILENAME=%REPO_NAME%_CONFIG_list_so_ss_current
+set LOG_FILENAME=
+rem echo LOG_FILENAME: %LOG_FILENAME%
 
 rem ...Каталог BAT_DIR: каталог
 if "%BAT_DIR%" == "" (
@@ -57,13 +60,6 @@ if "%LOG_OPT%" == "" (
     set LOG_OPT=11
 )
 rem echo LOG_OPT: %LOG_OPT%
-rem -------------------------------------------------------------------
-rem ...Каталог APP_KIX: каталог
-set KIX_DIR=
-rem ...Скрипт APP_KIX: имя+расширение
-set APP_KIX=lyrxxx_ШАБЛОН.kix
-rem -------------------------------------------------------------------
-
 rem echo -------------------------------------------------------
 rem echo 1.PROJECTS
 rem echo -------------------------------------------------------
@@ -118,7 +114,7 @@ if "%LOG_FILENAME%" == "" (
 )
 rem echo LOG_FILENAME: %LOG_FILENAME%
 rem ...Файл журнала: каталог+имя+расширение
-set LOG_FULLFILENAME=%LOG_DIR%\%REPO_NAME%_%LOG_FILENAME%.log
+set LOG_FULLFILENAME=%LOG_DIR%\%LOG_FILENAME%.log
 rem echo LOG_FULLFILENAME: %LOG_FULLFILENAME%
 rem ...Параметры журнала LOG_OPT1
 set LOG_OPT1=%LOG_OPT:~0,1%
@@ -132,54 +128,17 @@ if "%LOG_OPT2%" == "" (
     set LOG_OPT2=1
 )
 rem echo LOG_OPT2: %LOG_OPT2%
-rem echo -------------------------------------------------------
-rem echo 3.БИБЛИОТЕКА LYR
-rem echo -------------------------------------------------------
-rem ...Каталог библиотеки KIX: каталог
-if "%LIB_KIX%" == "" (
-    rem echo Каталог LIB_KIX не установлен
-    if "%COMPUTERNAME%" == "%USERDOMAIN%" (
-        set LIB_KIX=%PROJECTS_LYR_DIR%\CHECK_LIST\03_SCRIPT\01_KIX\%PROJECTS%\TOOLS_KIX\LIB
-        set LIB_KIX=D:\TOOLS\TOOLS_BAT\TOOLS_KIX\LIB
-    ) else (
-        set LIB_KIX=\\S73FS01\APPInfo\tools\LIB
-    )
-)
-rem echo LIB_KIX: %LIB_KIX%
-if exist %LIB_KIX% (
-    rem echo Каталог %LIB_KIX% существует
-) else (
-    echo ERROR: Каталог %LIB_KIX% не существует
-    exit /b 1
-)
-rem echo -------------------------------------------------------
-rem echo 4.APP_KIX
-rem echo -------------------------------------------------------
-if exist %APP_KIX% (
-    rem echo Файл %APP_KIX% существует
-    set KIX_DIR=.\
-) else (
-    rem echo Файл %APP_KIX% не существует
-    if exist KIX\%APP_KIX% (
-        rem echo Файл KIX\%APP_KIX% существует
-        set KIX_DIR=KIX
-    ) else (
-        rem echo Файл KIX\%APP_KIX% не существует
-        if exist %BAT_DIR%\KIX\%APP_KIX% (
-            rem echo Файл %BAT_DIR%\KIX\%APP_KIX% существует
-            set KIX_DIR=%BAT_DIR%\KIX
-        ) else (
-            echo ERROR: Файл %BAT_DIR%\KIX\%APP_KIX% не существует
-            exit /b 1
-        )
-    )
-)
-rem echo KIX_DIR: %KIX_DIR%
 
 echo -------------------------------------------------------
-echo 5. Запуск APP_KIX
+echo 5. Запуск
 echo -------------------------------------------------------
-kix32.exe %KIX_DIR%\%APP_KIX% "$P1=%1" "$P2=%2" "$P3=%3" "$P4=%4" "$P5=%5" "$P6=%6" "$P7=%7" "$P8=%8" "$P9=%9"
+
+:begin
+echo --------------------------------------------------------------- > %LOG_FULLFILENAME%
+echo ...%SCRIPT_FULLFILENAME% >> %LOG_FULLFILENAME%
+echo --------------------------------------------------------------- >> %LOG_FULLFILENAME%
+
+far -v %LOG_FULLFILENAME%
 
 exit /b 0
 
