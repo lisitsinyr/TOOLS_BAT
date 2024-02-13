3104ff86102047ebce138a9ea4491a3c7a4e17f7
lisitsinyr <lisitsinyr@gmail.com>
2024-02-13 20:27:20 +0400

    20240213202720

--- BAT/UPDATE_PROJECTS_GIT_WORK.bat
+++ BAT/UPDATE_PROJECTS_GIT_WORK.bat
@@ -35,250 +35,116 @@ echo Текущий каталог %CURRENT_DIR% >> %LOG_FULLFILENAME%
 
 echo ТЕЛО СКРИПТА %SCRIPT_BASEFILENAME% ... >> %LOG_FULLFILENAME%
 
-rem ---------------------------------------------------------
-rem COPY: D:\PROJECTS_LYR\CHECK_LIST\07_GIT\PROJECTS_GIT\TOOLS_GIT\BAT\A.WORK\*.bat
-rem  -> D:\PROJECTS_LYR\CHECK_LIST\07_GIT\PROJECTS_GIT
-rem ---------------------------------------------------------
+set UPDATE_BAT=D:\PROJECTS_LYR\CHECK_LIST\03_SCRIPT\04_BAT\TOOLS_BAT\BAT
+
 set DIR_FROM=D:\PROJECTS_LYR\CHECK_LIST\07_GIT\PROJECTS_GIT\TOOLS_GIT\BAT\A.WORK
-echo DIR_FROM: %DIR_FROM%
-set DIR_TO=D:\PROJECTS_LYR\CHECK_LIST\07_GIT\PROJECTS_GIT
-echo DIR_TO: %DIR_TO%
+echo DIR_FROM: %DIR_FROM% >> %LOG_FULLFILENAME%
 set FILES=*.bat
-echo FILES: %FILES%
-for /R %DIR_FROM% %%f in (%FILES%) do (
-    echo %%~nf%%~xf
-    copy %%f %DIR_TO%\
-)
-rem ---------------------------------------------------------
-rem COPY: D:\PROJECTS_LYR\CHECK_LIST\07_GIT\PROJECTS_GIT\TOOLS_GIT\BAT\A.WORK\*.bat
-rem  -> D:\PROJECTS_LYR\CHECK_LIST\07_GIT\TOOLS_GIT
-rem ---------------------------------------------------------
-set DIR_FROM=D:\PROJECTS_LYR\CHECK_LIST\07_GIT\PROJECTS_GIT\TOOLS_GIT\BAT\A.WORK
-echo DIR_FROM: %DIR_FROM%
-set DIR_TO=D:\PROJECTS_LYR\CHECK_LIST\07_GIT\TOOLS_GIT
-echo DIR_TO: %DIR_TO%
-set FILES=*.bat
-echo FILES: %FILES%
-for /R %DIR_FROM% %%f in (%FILES%) do (
-    echo %%~nf%%~xf
-    copy %%f %DIR_TO%\
-)
-rem ---------------------------------------------------------
-rem COPY: D:\PROJECTS_LYR\CHECK_LIST\07_GIT\PROJECTS_GIT\TOOLS_GIT\BAT\A.WORK\*.bat
-rem  -> D:\PROJECTS_LYR\CHECK_LIST\01_OS\03_UNIX\PROJECTS_UNIX
-rem ---------------------------------------------------------
-set DIR_FROM=D:\PROJECTS_LYR\CHECK_LIST\07_GIT\PROJECTS_GIT\TOOLS_GIT\BAT\A.WORK
-echo DIR_FROM: %DIR_FROM%
-set DIR_TO=D:\PROJECTS_LYR\CHECK_LIST\01_OS\03_UNIX\PROJECTS_UNIX
-echo DIR_TO: %DIR_TO%
-set FILES=*.bat
-echo FILES: %FILES%
-for /R %DIR_FROM% %%f in (%FILES%) do (
-    echo %%~nf%%~xf
-    copy %%f %DIR_TO%\
-)
-rem ---------------------------------------------------------
-rem COPY: D:\PROJECTS_LYR\CHECK_LIST\07_GIT\PROJECTS_GIT\TOOLS_GIT\BAT\A.WORK\*.bat
-rem  -> D:\PROJECTS_LYR\CHECK_LIST\01_OS\03_UNIX\TOOLS_SH
-rem ---------------------------------------------------------
-set DIR_FROM=D:\PROJECTS_LYR\CHECK_LIST\07_GIT\PROJECTS_GIT\TOOLS_GIT\BAT\A.WORK
-echo DIR_FROM: %DIR_FROM%
-set DIR_TO=D:\PROJECTS_LYR\CHECK_LIST\01_OS\03_UNIX\TOOLS_SH
-echo DIR_TO: %DIR_TO%
-set FILES=*.bat
-echo FILES: %FILES%
-for /R %DIR_FROM% %%f in (%FILES%) do (
-    echo %%~nf%%~xf
-    copy %%f %DIR_TO%\
-)
-rem ---------------------------------------------------------
-rem COPY: D:\PROJECTS_LYR\CHECK_LIST\07_GIT\PROJECTS_GIT\TOOLS_GIT\BAT\A.WORK\*.bat
-rem  -> D:\PROJECTS_LYR\CHECK_LIST\03_SCRIPT\01_KIX\PROJECTS_KIX
-rem ---------------------------------------------------------
-set DIR_FROM=D:\PROJECTS_LYR\CHECK_LIST\07_GIT\PROJECTS_GIT\TOOLS_GIT\BAT\A.WORK
-echo DIR_FROM: %DIR_FROM%
-set DIR_TO=D:\PROJECTS_LYR\CHECK_LIST\03_SCRIPT\01_KIX\PROJECTS_KIX
-echo DIR_TO: %DIR_TO%
-set FILES=*.bat
-echo FILES: %FILES%
-for /R %DIR_FROM% %%f in (%FILES%) do (
-    echo %%~nf%%~xf
-    copy %%f %DIR_TO%\
-)
-rem ---------------------------------------------------------
-rem COPY: D:\PROJECTS_LYR\CHECK_LIST\07_GIT\PROJECTS_GIT\TOOLS_GIT\BAT\A.WORK\*.bat
-rem  -> D:\PROJECTS_LYR\CHECK_LIST\03_SCRIPT\01_KIX\TOOLS_KIX
-rem ---------------------------------------------------------
-set DIR_FROM=D:\PROJECTS_LYR\CHECK_LIST\07_GIT\PROJECTS_GIT\TOOLS_GIT\BAT\A.WORK
-echo DIR_FROM: %DIR_FROM%
-set DIR_TO=D:\PROJECTS_LYR\CHECK_LIST\03_SCRIPT\01_KIX\TOOLS_KIX
-echo DIR_TO: %DIR_TO%
-set FILES=*.bat
-echo FILES: %FILES%
-for /R %DIR_FROM% %%f in (%FILES%) do (
-    echo %%~nf%%~xf
-    copy %%f %DIR_TO%\
-)
-rem ---------------------------------------------------------
-rem COPY: D:\PROJECTS_LYR\CHECK_LIST\07_GIT\PROJECTS_GIT\TOOLS_GIT\BAT\A.WORK\*.bat
-rem  -> D:\PROJECTS_LYR\CHECK_LIST\03_SCRIPT\04_BAT\PROJECTS_BAT
-rem ---------------------------------------------------------
-set DIR_FROM=D:\PROJECTS_LYR\CHECK_LIST\07_GIT\PROJECTS_GIT\TOOLS_GIT\BAT\A.WORK
-echo DIR_FROM: %DIR_FROM%
-set DIR_TO=D:\PROJECTS_LYR\CHECK_LIST\03_SCRIPT\04_BAT\PROJECTS_BAT
-echo DIR_TO: %DIR_TO%
-set FILES=*.bat
-echo FILES: %FILES%
-for /R %DIR_FROM% %%f in (%FILES%) do (
-    echo %%~nf%%~xf
-    copy %%f %DIR_TO%\
-)
-rem ---------------------------------------------------------
-rem COPY: D:\PROJECTS_LYR\CHECK_LIST\07_GIT\PROJECTS_GIT\TOOLS_GIT\BAT\A.WORK\*.bat
-rem  -> D:\PROJECTS_LYR\CHECK_LIST\03_SCRIPT\04_BAT\TOOLS_BAT
-rem ---------------------------------------------------------
-set DIR_FROM=D:\PROJECTS_LYR\CHECK_LIST\07_GIT\PROJECTS_GIT\TOOLS_GIT\BAT\A.WORK
-echo DIR_FROM: %DIR_FROM%
-set DIR_TO=D:\PROJECTS_LYR\CHECK_LIST\03_SCRIPT\04_BAT\TOOLS_BAT
-echo DIR_TO: %DIR_TO%
-set FILES=*.bat
-echo FILES: %FILES%
-for /R %DIR_FROM% %%f in (%FILES%) do (
-    echo %%~nf%%~xf
-    copy %%f %DIR_TO%\
-)
-rem ---------------------------------------------------------
-rem COPY: D:\PROJECTS_LYR\CHECK_LIST\07_GIT\PROJECTS_GIT\TOOLS_GIT\BAT\A.WORK\*.bat
-rem  -> D:\PROJECTS_LYR\CHECK_LIST\05_DESKTOP\02_Python\PROJECTS_PY\EXAMPLES_PY
-rem ---------------------------------------------------------
-set DIR_FROM=D:\PROJECTS_LYR\CHECK_LIST\07_GIT\PROJECTS_GIT\TOOLS_GIT\BAT\A.WORK
-echo DIR_FROM: %DIR_FROM%
-set DIR_TO=D:\PROJECTS_LYR\CHECK_LIST\05_DESKTOP\02_Python\PROJECTS_PY\EXAMPLES_PY
-echo DIR_TO: %DIR_TO%
-set FILES=*.bat
-echo FILES: %FILES%
-for /R %DIR_FROM% %%f in (%FILES%) do (
-    echo %%~nf%%~xf
-    copy %%f %DIR_TO%\
-)
-rem ---------------------------------------------------------
-rem COPY: D:\PROJECTS_LYR\CHECK_LIST\07_GIT\PROJECTS_GIT\TOOLS_GIT\BAT\A.WORK\*.bat
-rem  -> D:\PROJECTS_LYR\CHECK_LIST\05_DESKTOP\02_Python\PROJECTS_PY\MobileAPP
-rem ---------------------------------------------------------
-set DIR_FROM=D:\PROJECTS_LYR\CHECK_LIST\07_GIT\PROJECTS_GIT\TOOLS_GIT\BAT\A.WORK
-echo DIR_FROM: %DIR_FROM%
-set DIR_TO=D:\PROJECTS_LYR\CHECK_LIST\05_DESKTOP\02_Python\PROJECTS_PY\MobileAPP
-echo DIR_TO: %DIR_TO%
-set FILES=*.bat
-echo FILES: %FILES%
-for /R %DIR_FROM% %%f in (%FILES%) do (
-    echo %%~nf%%~xf
-    copy %%f %DIR_TO%\
-)
-rem ---------------------------------------------------------
-rem COPY: D:\PROJECTS_LYR\CHECK_LIST\07_GIT\PROJECTS_GIT\TOOLS_GIT\BAT\A.WORK\*.bat
-rem  -> D:\PROJECTS_LYR\CHECK_LIST\05_DESKTOP\02_Python\PROJECTS_PY\PROJECTS_PY
-rem ---------------------------------------------------------
-set DIR_FROM=D:\PROJECTS_LYR\CHECK_LIST\07_GIT\PROJECTS_GIT\TOOLS_GIT\BAT\A.WORK
-echo DIR_FROM: %DIR_FROM%
-set DIR_TO=D:\PROJECTS_LYR\CHECK_LIST\05_DESKTOP\02_Python\PROJECTS_PY\PROJECTS_PY
-echo DIR_TO: %DIR_TO%
-set FILES=*.bat
-echo FILES: %FILES%
-for /R %DIR_FROM% %%f in (%FILES%) do (
-    echo %%~nf%%~xf
-    copy %%f %DIR_TO%\
-)
-rem ---------------------------------------------------------
-rem COPY: D:\PROJECTS_LYR\CHECK_LIST\07_GIT\PROJECTS_GIT\TOOLS_GIT\BAT\A.WORK\*.bat
-rem  -> D:\PROJECTS_LYR\CHECK_LIST\05_DESKTOP\02_Python\PROJECTS_PY\TESTS_PY
-rem ---------------------------------------------------------
-set DIR_FROM=D:\PROJECTS_LYR\CHECK_LIST\07_GIT\PROJECTS_GIT\TOOLS_GIT\BAT\A.WORK
-echo DIR_FROM: %DIR_FROM%
-set DIR_TO=D:\PROJECTS_LYR\CHECK_LIST\05_DESKTOP\02_Python\PROJECTS_PY\TESTS_PY
-echo DIR_TO: %DIR_TO%
-set FILES=*.bat
-echo FILES: %FILES%
-for /R %DIR_FROM% %%f in (%FILES%) do (
-    echo %%~nf%%~xf
-    copy %%f %DIR_TO%\
-)
-rem ---------------------------------------------------------
-rem COPY: D:\PROJECTS_LYR\CHECK_LIST\07_GIT\PROJECTS_GIT\TOOLS_GIT\BAT\A.WORK\*.bat
-rem  -> D:\PROJECTS_LYR\CHECK_LIST\05_DESKTOP\02_Python\PROJECTS_PY\TOOLS_PY
-rem ---------------------------------------------------------
-set DIR_FROM=D:\PROJECTS_LYR\CHECK_LIST\07_GIT\PROJECTS_GIT\TOOLS_GIT\BAT\A.WORK
-echo DIR_FROM: %DIR_FROM%
-set DIR_TO=D:\PROJECTS_LYR\CHECK_LIST\05_DESKTOP\02_Python\PROJECTS_PY\TOOLS_PY
-echo DIR_TO: %DIR_TO%
-set FILES=*.bat
-echo FILES: %FILES%
-for /R %DIR_FROM% %%f in (%FILES%) do (
-    echo %%~nf%%~xf
-    copy %%f %DIR_TO%\
-)
-rem ---------------------------------------------------------
-rem COPY: D:\PROJECTS_LYR\CHECK_LIST\07_GIT\PROJECTS_GIT\TOOLS_GIT\BAT\A.WORK\*.bat
-rem  -> D:\PROJECTS_LYR\CHECK_LIST\05_DESKTOP\02_Python\PROJECTS_PY\YOUTUBE
-rem ---------------------------------------------------------
-set DIR_FROM=D:\PROJECTS_LYR\CHECK_LIST\07_GIT\PROJECTS_GIT\TOOLS_GIT\BAT\A.WORK
-echo DIR_FROM: %DIR_FROM%
-set DIR_TO=D:\PROJECTS_LYR\CHECK_LIST\05_DESKTOP\02_Python\PROJECTS_PY\YOUTUBE
-echo DIR_TO: %DIR_TO%
-set FILES=*.bat
-echo FILES: %FILES%
-for /R %DIR_FROM% %%f in (%FILES%) do (
-    echo %%~nf%%~xf
-    copy %%f %DIR_TO%\
-)
-rem ---------------------------------------------------------
-rem COPY: D:\PROJECTS_LYR\CHECK_LIST\07_GIT\PROJECTS_GIT\TOOLS_GIT\BAT\A.WORK\*.bat
-rem  -> D:\PROJECTS_LYR\CHECK_LIST\05_DESKTOP\03_Java\PROJECTS_JAVA\PROJECTS_JAVA
-rem ---------------------------------------------------------
-set DIR_FROM=D:\PROJECTS_LYR\CHECK_LIST\07_GIT\PROJECTS_GIT\TOOLS_GIT\BAT\A.WORK
-echo DIR_FROM: %DIR_FROM%
-set DIR_TO=D:\PROJECTS_LYR\CHECK_LIST\05_DESKTOP\03_Java\PROJECTS_JAVA\PROJECTS_JAVA
-echo DIR_TO: %DIR_TO%
-set FILES=*.bat
-echo FILES: %FILES%
-for /R %DIR_FROM% %%f in (%FILES%) do (
-    echo %%~nf%%~xf
-    copy %%f %DIR_TO%\
-)
-rem ---------------------------------------------------------
-rem COPY: D:\PROJECTS_LYR\CHECK_LIST\07_GIT\PROJECTS_GIT\TOOLS_GIT\BAT\A.WORK\*.bat
-rem  -> D:\PROJECTS_LYR\CHECK_LIST\05_DESKTOP\03_Java\PROJECTS_JAVA\TESTS_JAVA
-rem ---------------------------------------------------------
-set DIR_FROM=D:\PROJECTS_LYR\CHECK_LIST\07_GIT\PROJECTS_GIT\TOOLS_GIT\BAT\A.WORK
-echo DIR_FROM: %DIR_FROM%
-set DIR_TO=D:\PROJECTS_LYR\CHECK_LIST\05_DESKTOP\03_Java\PROJECTS_JAVA\TESTS_JAVA
-echo DIR_TO: %DIR_TO%
-set FILES=*.bat
-echo FILES: %FILES%
-for /R %DIR_FROM% %%f in (%FILES%) do (
-    echo %%~nf%%~xf
-    copy %%f %DIR_TO%\
-)
-rem ---------------------------------------------------------
-rem COPY: D:\PROJECTS_LYR\CHECK_LIST\07_GIT\PROJECTS_GIT\TOOLS_GIT\BAT\A.WORK\*.bat
-rem  -> D:\PROJECTS_LYR\CHECK_LIST\05_DESKTOP\03_Java\PROJECTS_JAVA\TOOLS_JAVA
-rem ---------------------------------------------------------
-set DIR_FROM=D:\PROJECTS_LYR\CHECK_LIST\07_GIT\PROJECTS_GIT\TOOLS_GIT\BAT\A.WORK
-echo DIR_FROM: %DIR_FROM%
-set DIR_TO=D:\PROJECTS_LYR\CHECK_LIST\05_DESKTOP\03_Java\PROJECTS_JAVA\TOOLS_JAVA
-echo DIR_TO: %DIR_TO%
-set FILES=*.bat
-echo FILES: %FILES%
-for /R %DIR_FROM% %%f in (%FILES%) do (
-    echo %%~nf%%~xf
-    copy %%f %DIR_TO%\
-)
+echo FILES: %FILES% >> %LOG_FULLFILENAME%
+
+call :REPO_WORK PROJECTS_GIT  || exit /b 1
+call :REPO_WORK TOOLS_GIT     || exit /b 1
+call :REPO_WORK PROJECTS_UNIX || exit /b 1
+call :REPO_WORK TOOLS_SH      || exit /b 1
+call :REPO_WORK PROJECTS_KIX  || exit /b 1
+call :REPO_WORK TOOLS_KIX     || exit /b 1
+call :REPO_WORK PROJECTS_BAT  || exit /b 1
+call :REPO_WORK TOOLS_BAT     || exit /b 1
+call :REPO_WORK EXAMPLES_PY   || exit /b 1
+call :REPO_WORK MobileAPP     || exit /b 1
+call :REPO_WORK PROJECTS_PY   || exit /b 1
+call :REPO_WORK TESTS_PY      || exit /b 1
+call :REPO_WORK TOOLS_PY      || exit /b 1
+call :REPO_WORK YOUTUBE       || exit /b 1
+call :REPO_WORK PROJECTS_JAVA || exit /b 1
+call :REPO_WORK TESTS_JAVA    || exit /b 1
+call :REPO_WORK TOOLS_JAVA    || exit /b 1
 
 cd /D %CURRENT_DIR%
 echo Текущий каталог %CURRENT_DIR% >> %LOG_FULLFILENAME%
 
 rem far -v %LOG_FULLFILENAME%
 
+:Exit
+
+rem Выход из сценария. Дальше - только функции.
 exit /b 0
 
-:Exit
+rem Функция для чтения настроек из файла.
+rem Вход: %1 - Имя файла с настройками
+
+:REPO_WORK
+set PROJECTS=%1
+if "%PROJECTS%" == "PROJECTS_GIT" (
+    set DIR_TO=D:\PROJECTS_LYR\CHECK_LIST\07_GIT\PROJECTS_GIT
+) else (
+if "%PROJECTS%" == "TOOLS_GIT" (
+    set DIR_TO=D:\PROJECTS_LYR\CHECK_LIST\07_GIT\TOOLS_GIT
+) else (
+if "%PROJECTS%" == "PROJECTS_UNIX" (
+    set DIR_TO=D:\PROJECTS_LYR\CHECK_LIST\01_OS\03_UNIX\PROJECTS_UNIX
+) else (
+if "%PROJECTS%" == "TOOLS_SH" (
+    set DIR_TO=D:\PROJECTS_LYR\CHECK_LIST\01_OS\03_UNIX\TOOLS_SH
+) else (
+if "%PROJECTS%" == "PROJECTS_KIX" (
+    set DIR_TO=D:\PROJECTS_LYR\CHECK_LIST\03_SCRIPT\01_KIX\PROJECTS_KIX
+) else (
+if "%PROJECTS%" == "TOOLS_KIX" (
+    set DIR_TO=D:\PROJECTS_LYR\CHECK_LIST\03_SCRIPT\01_KIX\TOOLS_KIX
+) else (
+if "%PROJECTS%" == "PROJECTS_BAT" (
+    set DIR_TO=D:\PROJECTS_LYR\CHECK_LIST\03_SCRIPT\04_BAT\PROJECTS_BAT
+) else (
+if "%PROJECTS%" == "TOOLS_BAT" (
+    set DIR_TO=D:\PROJECTS_LYR\CHECK_LIST\03_SCRIPT\04_BAT\TOOLS_BAT
+) else (
+if "%PROJECTS%" == "EXAMPLES_PY" (
+    set DIR_TO=D:\PROJECTS_LYR\CHECK_LIST\05_DESKTOP\02_Python\PROJECTS_PY\EXAMPLES_PY
+) else (
+if "%PROJECTS%" == "MobileAPP" (
+    set DIR_TO=D:\PROJECTS_LYR\CHECK_LIST\05_DESKTOP\02_Python\PROJECTS_PY\MobileAPP
+) else (
+if "%PROJECTS%" == "PROJECTS_PY" (
+    set DIR_TO=D:\PROJECTS_LYR\CHECK_LIST\05_DESKTOP\02_Python\PROJECTS_PY\PROJECTS_PY
+) else (
+if "%PROJECTS%" == "TESTS_PY" (
+    set DIR_TO=D:\PROJECTS_LYR\CHECK_LIST\05_DESKTOP\02_Python\PROJECTS_PY\TESTS_PY
+) else (
+if "%PROJECTS%" == "TOOLS_PY" (
+    set DIR_TO=D:\PROJECTS_LYR\CHECK_LIST\05_DESKTOP\02_Python\PROJECTS_PY\TOOLS_PY
+) else (
+if "%PROJECTS%" == "YOUTUBE" (
+    set DIR_TO=D:\PROJECTS_LYR\CHECK_LIST\05_DESKTOP\02_Python\PROJECTS_PY\YOUTUBE
+) else (
+if "%PROJECTS%" == "PROJECTS_JAVA" (
+    set DIR_TO=D:\PROJECTS_LYR\CHECK_LIST\05_DESKTOP\03_Java\PROJECTS_JAVA\PROJECTS_JAVA
+) else (
+if "%PROJECTS%" == "TESTS_JAVA" (
+    set DIR_TO=D:\PROJECTS_LYR\CHECK_LIST\05_DESKTOP\03_Java\PROJECTS_JAVA\TESTS_JAVA
+) else (
+if "%PROJECTS%" == "TOOLS_JAVA" (
+    set DIR_TO=D:\PROJECTS_LYR\CHECK_LIST\05_DESKTOP\03_Java\PROJECTS_JAVA\TOOLS_JAVA
+) else (
+    set DIR_TO=
+)))))))))))))))))
+
+if not "%PROJECTS%" == "" (
+    echo --------------------------------------------------------- >> %LOG_FULLFILENAME%
+    echo PROJECTS: %PROJECTS% >> %LOG_FULLFILENAME%
+    echo DIR_TO: %DIR_TO% >> %LOG_FULLFILENAME%
+    echo --------------------------------------------------------- >> %LOG_FULLFILENAME%
+    rem for /R %DIR_FROM% %%f in (%FILES%) do (
+    rem     echo %%~nf%%~xf
+    rem     copy %%f %DIR_TO%\
+    rem )
+
+    del %DIR_TO%\*.bat
+    echo COPY: %DIR_FROM%\lyrgit_push_main.bat       >> %LOG_FULLFILENAME%
+    copy %DIR_FROM%\lyrgit_push_main.bat %DIR_TO%    >> %LOG_FULLFILENAME%
+    echo COPY: %UPDATE_BAT%\UPDATE_%PROJECTS%.bat    >> %LOG_FULLFILENAME%
+    copy %UPDATE_BAT%\UPDATE_%PROJECTS%.bat %DIR_TO% >> %LOG_FULLFILENAME%
+    echo --------------------------------------------------------- >> %LOG_FULLFILENAME%
+)
+
+exit /b 0
