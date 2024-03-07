482d7b1bee1f6ba59d156ac3556dfc09c9f2acbc
lisitsinyr <lisitsinyr@gmail.com>
2024-03-07 17:09:11 +0400

    20240307170910

--- BAT/diff.bat
+++ BAT/diff.bat
@@ -1,13 +1,16 @@
-@echo on
+@echo off
 rem -------------------------------------------------------------------
-rem diff2.bat
+rem diff1.bat
 rem -------------------------------------------------------------------
 chcp 1251>NUL
 
 setlocal enabledelayedexpansion
 
 :begin
+    rem windiff.exe %1 %2
+    rem WinMergeU.exe %1 %2
     wincmp3.exe %1 %2
+
 :Exit
 exit /b 0
 
