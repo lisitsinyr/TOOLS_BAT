237b8ba10101db6de483a11464bcef4103862110
lisitsinyr <lisitsinyr@gmail.com>
2024-03-21 18:02:42 +0400

    20240321180242

---
+++ BAT/lyrgit_push_main.bat
@@ -0,0 +1,318 @@
+@echo off
+rem -------------------------------------------------------------------
+rem lyrgit_push_main.bat
+rem ----------------------------------------------------------------------------
+rem ***Отправить изменения
+rem ----------------------------------------------------------------------------
+rem usage: git push [<options>] [<repository> [<refspec>...]]
+rem 
+rem     -v, --verbose         be more verbose
+rem     -q, --quiet           be more quiet
+rem     --repo <repository>   repository
+rem     --all                 push all refs
+rem     --mirror              mirror all refs
+rem     -d, --delete          delete refs
+rem     --tags                push tags (can't be used with --all or --mirror)
+rem     -n, --dry-run         dry run
+rem     --porcelain           machine-readable output
+rem     -f, --force           force updates
+rem     --force-with-lease[=<refname>:<expect>]
+rem                           require old value of ref to be at this value
+rem     --force-if-includes   require remote updates to be integrated locally
+rem     --recurse-submodules (check|on-demand|no)
+rem                           control recursive pushing of submodules
+rem     --thin                use thin pack
+rem     --receive-pack <receive-pack>
+rem                           receive pack program
+rem     --exec <receive-pack>
+rem                           receive pack program
+rem     -u, --set-upstream    set upstream for git pull/status
+rem     --progress            force progress reporting
+rem     --prune               prune locally removed refs
+rem     --no-verify           bypass pre-push hook
+rem     --follow-tags         push missing but relevant tags
+rem     --signed[=(yes|no|if-asked)]
+rem                           GPG sign the push
+rem     --atomic              request atomic transaction on remote side
+rem     -o, --push-option <server-specific>
+rem                           option to transmit
+rem     -4, --ipv4            use IPv4 addresses only
+rem     -6, --ipv6            use IPv6 addresses only
+rem -------------------------------------------------------------------
+chcp 1251>NUL
+
+setlocal enabledelayedexpansion
+
+rem --------------------------------------------------------------------------------
+rem 
+rem --------------------------------------------------------------------------------
+:begin
+    call :MAIN_INIT %0 0 || exit /b 1
+    call :MAIN_SET || exit /b 1
+    call :StartLogFile || exit /b 1
+    rem set DIR_SAVE=%CURRENT_DIR%
+    rem call :MAIN_CHECK_PARAMETR || exit /b 1
+    rem call :MAIN_SYNTAX || exit /b 1
+    call :MAIN || exit /b 1
+    call :StopLogFile || exit /b 1
+    rem far -v %LOG_FULLFILENAME%
+    rem cd /D %DIR_SAVE%
+:Exit
+exit /b 0
+rem --------------------------------------------------------------------------------
+
+rem =================================================
+rem ФУНКЦИИ LIB
+rem =================================================
+:__SET_VAR_SCRIPT
+%LIB_BAT%\__SET_LIB.bat %*
+exit /b 0
+:__SET_VAR_DEFAULT
+%LIB_BAT%\__SET_LIB.bat %*
+exit /b 0
+:__SET_VAR_PROJECTS
+%LIB_BAT%\__SET_LIB.bat %*
+exit /b 0
+:__SET_CHECK_REPO
+%LIB_BAT%\__SET_LIB.bat %*
+exit /b 0
+:__SET_LOG
+%LIB_BAT%\__SET_LIB.bat %*
+exit /b 0
+:FormatStr
+%LIB_BAT%\LYRLog.bat %*
+exit /b 0
+:AddLog
+%LIB_BAT%\LYRLog.bat %*
+exit /b 0
+:AddLogFile
+%LIB_BAT%\LYRLog.bat %*
+exit /b 0
+:StartLogFile
+%LIB_BAT%\LYRLog.bat %*
+exit /b 0
+:StopLogFile
+%LIB_BAT%\LYRLog.bat %*
+exit /b 0
+:Check_P
+%LIB_BAT%\LYRSupport.bat %*
+exit /b 0
+:Pause
+%LIB_BAT%\LYRSupport.bat %*
+exit /b 0
+:PressAnyKey
+%LIB_BAT%\LYRSupport.bat %*
+exit /b 0
+
+rem -----------------------------------------------
+rem procedure MAIN_INIT (FULLFILENAME, DEBUG)
+rem -----------------------------------------------
+:MAIN_INIT
+rem beginfunction
+    rem  -------------------------------------------------------------------
+    rem  DEBUG 1-включить DEBUG 0-выключить DEBUG
+    set DEBUG=%2
+    rem echo DEBUG: %DEBUG%
+    set FUNCNAME=%0
+    if "%DEBUG%"=="1" (
+        echo DEBUG: procedure %FUNCNAME% ...
+    )
+
+    rem -------------------------------------------------------------------
+    rem PROJECTS - имя проекта
+    rem -------------------------------------------------------------------
+    set PROJECTS=PROJECTS_BAT
+
+    rem -------------------------------------------------------------------
+    rem PROJECTS_LYR_DIR - каталог проектов
+    rem -------------------------------------------------------------------
+    set PROJECTS_LYR_DIR=D:\PROJECTS_LYR
+    rem -------------------------------------------------------------------
+    rem SCRIPTS_DIR - Каталог скриптов
+    rem -------------------------------------------------------------------
+    if "%SCRIPTS_DIR%" == "" (
+        set SCRIPTS_DIR=D:\TOOLS\TOOLS_BAT
+        set SCRIPTS_DIR=D:\PROJECTS_LYR\CHECK_LIST\03_SCRIPT\04_BAT\TOOLS_BAT
+        set SCRIPTS_DIR=D:\PROJECTS_LYR\CHECK_LIST\03_SCRIPT\04_BAT\PROJECTS_BAT\TOOLS_BAT
+    )
+    rem -------------------------------------------------------------------
+    rem SCRIPT_FULLFILENAME - Файл скрипта [каталог+имя+расширение]
+    rem -------------------------------------------------------------------
+    set SCRIPT_FULLFILENAME=%1
+    rem echo PROJECTS_LYR_DIR: %PROJECTS_LYR_DIR%
+    rem echo SCRIPTS_DIR: %SCRIPTS_DIR%
+    rem echo SCRIPT_FULLFILENAME: %SCRIPT_FULLFILENAME%
+  
+    rem -------------------------------------------------------------------
+    rem PROJECTS_DIR - каталог проекта
+    rem -------------------------------------------------------------------
+    set PROJECTS_DIR=%PROJECTS_LYR_DIR%\CHECK_LIST\03_SCRIPT\04_BAT\%PROJECTS%
+    rem echo PROJECTS_DIR: %PROJECTS_DIR%
+
+    rem -------------------------------------------------------------------
+    rem LIB_BAT - каталог библиотеки скриптов
+    rem -------------------------------------------------------------------
+    if "%LIB_BAT%" == "" (
+        set LIB_BAT=%SCRIPTS_DIR%\LIB
+        rem echo LIB_BAT: %LIB_BAT%
+    )
+    if not exist %LIB_BAT%\ (
+        echo ERROR: Каталог библиотеки LYR $LIB_BAT не существует...
+        exit /b 0
+    )
+
+    rem -------------------------------------------------------------------
+    rem SCRIPTS_DIR_KIX - Каталог скриптов KIX
+    rem -------------------------------------------------------------------
+    if "%SCRIPTS_DIR_KIX%" == "" (
+        set SCRIPTS_DIR_KIX=D:\TOOLS\TOOLS_KIX
+        set SCRIPTS_DIR_KIX=D:\PROJECTS_LYR\CHECK_LIST\03_SCRIPT\01_KIX\TOOLS_KIX
+        set SCRIPTS_DIR_KIX=D:\PROJECTS_LYR\CHECK_LIST\03_SCRIPT\01_KIX\PROJECTS_KIX\TOOLS_KIX
+    )
+    rem echo SCRIPTS_DIR_KIX: %SCRIPTS_DIR_KIX%
+
+    exit /b 0
+rem endfunction
+
+rem -----------------------------------------------
+rem procedure MAIN_SET ()
+rem -----------------------------------------------
+:MAIN_SET
+rem beginfunction
+    set FUNCNAME=%0
+    if "%DEBUG%"=="1" (
+        echo DEBUG: procedure %FUNCNAME% ...
+    )
+
+    call :__SET_VAR_DEFAULT %DEBUG% || exit /b 1
+    call :__SET_VAR_SCRIPT %SCRIPT_FULLFILENAME% || exit /b 1
+    call :__SET_VAR_PROJECTS || exit /b 1
+    call :__SET_CHECK_REPO || exit /b 1
+    rem -------------------------------------------------------------------
+    rem LOG_DT_FORMAT -
+    rem set LOG_DT_FORMAT=
+    rem -------------------------------------------------------------------
+    rem LOG_FILENAME_FORMAT - Формат имени файла журнала [FILENAME,DATETIME,...]
+    rem set LOG_FILENAME_FORMAT=
+    rem -------------------------------------------------------------------
+    rem LOG_FILE_ADD - Параметры журнала [0]
+    if "%LOG_FILE_ADD%"=="" set LOG_FILE_ADD=0
+    rem echo LOG_FILE_ADD: %LOG_FILE_ADD%
+    rem -------------------------------------------------------------------
+    rem LOG_FILE_DT - Параметры журнала [0]
+    if "%LOG_FILE_DT%"=="" set LOG_FILE_DT=0
+    rem  -------------------------------------------------------------------
+    rem LOG_DIR - Каталог журнала [каталог]
+    rem set LOG_DIR=
+    rem -------------------------------------------------------------------
+    rem LOG_FILENAME - Файл журнала [имя]
+    rem set LOG_FILENAME=
+    call :__SET_LOG || exit /b 1
+
+    exit /b 0
+rem endfunction
+
+rem --------------------------------------------------------------------------------
+rem procedure MAIN_CHECK_PARAMETR ()
+rem --------------------------------------------------------------------------------
+:MAIN_CHECK_PARAMETR
+rem beginfunction
+    set FUNCNAME=%0
+    if "%DEBUG%"=="1" (
+        echo DEBUG: procedure %FUNCNAME% ...
+    )
+
+    set P1=P1_default
+    call :Check_P P1 %1 || exit /b 1
+
+    rem call :AddLog %loStandard% %TEXT% "P1: %P1%" || exit /b 1
+    rem call :AddLog %loTextFile% %TEXT% "P1: %P1%" || exit /b 1
+    call :AddLog %loAll% %TEXT% P1: %P1% || exit /b 1
+    call :AddLog %loAll% %INFO% P1: %P1% || exit /b 1
+
+    rem set F=LYRLog.txt
+    rem call :AddLogFile %loAll% %F%
+
+    exit /b 0
+rem endfunction
+
+rem --------------------------------------------------------------------------------
+rem procedure MAIN_SYNTAX ()
+rem --------------------------------------------------------------------------------
+:MAIN_SYNTAX
+rem beginfunction
+    set FUNCNAME=%0
+    if "%DEBUG%"=="1" (
+        echo DEBUG: procedure %FUNCNAME% ...
+    )
+
+    exit /b 0
+rem endfunction
+
+rem =================================================
+rem procedure MAIN ()
+rem =================================================
+:MAIN
+rem beginfunction
+    set FUNCNAME=%0
+    if "%DEBUG%"=="1" (
+        echo DEBUG: procedure %FUNCNAME% ...
+    )
+
+    call :MAIN_FUNC || exit /b 1
+
+    rem call :Pause %SLEEP% || exit /b 1
+    rem call :PressAnyKey || exit /b 1
+
+    exit /b 0
+rem endfunction
+
+rem --------------------------------------------------------------------------------
+rem procedure MAIN_FUNC ()
+rem --------------------------------------------------------------------------------
+:MAIN_FUNC
+rem beginfunction
+    set FUNCNAME=%0
+    if "%DEBUG%"=="1" (
+        echo DEBUG: procedure %FUNCNAME% ...
+    )
+
+    call :GIT_RUN || exit /b 1
+
+    exit /b 0
+rem endfunction
+
+rem =================================================
+rem procedure GIT_RUN ()
+rem =================================================
+:GIT_RUN
+rem beginfunction
+    set FUNCNAME=%0
+    if "%DEBUG%"=="1" (
+        echo DEBUG: procedure %FUNCNAME% ...
+    )
+
+    set Comment="Git Bash commit update"
+    set Comment=%date:~6,4%%date:~3,2%%date:~0,2%%TIME:~0,2%%TIME:~3,2%%TIME:~6,2%
+
+    call :Check_P Comment %Comment% || exit /b 1
+    echo Comment: %Comment%
+
+    echo --------------------------------------------------------------- >> %LOG_FULLFILENAME%
+    echo ...git add --all >> %LOG_FULLFILENAME%
+    echo --------------------------------------------------------------- >> %LOG_FULLFILENAME%
+    git add --all >> %LOG_FULLFILENAME%
+    
+    echo --------------------------------------------------------------- >> %LOG_FULLFILENAME%
+    echo ...git commit -m "%Comment%" >> %LOG_FULLFILENAME%
+    echo --------------------------------------------------------------- >> %LOG_FULLFILENAME%
+    git commit -m "%Comment%" >> %LOG_FULLFILENAME%
+
+    echo --------------------------------------------------------------- >> %LOG_FULLFILENAME%
+    echo ...git push -u origin main >> %LOG_FULLFILENAME%
+    echo --------------------------------------------------------------- >> %LOG_FULLFILENAME%
+    git push -u origin main >> %LOG_FULLFILENAME%
+    echo --------------------------------------------------------------- >> %LOG_FULLFILENAME%
+
+    exit /b 0
+rem endfunction
