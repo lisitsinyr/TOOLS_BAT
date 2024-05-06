3f2020aef3d8c7940c925b03304269839fff7adf
lisitsinyr <lisitsinyr@gmail.com>
2024-05-06 09:22:36 +0400

    20240506

---
+++ BATPY/poetry_new.bat
@@ -0,0 +1,88 @@
+@echo off
+rem -------------------------------------------------------------------
+rem poetry_new.bat
+rem     Запуск poetry из глобального виртуального пространства
+rem     create our new project
+rem -------------------------------------------------------------------
+chcp 1251>NUL
+
+setlocal enabledelayedexpansion
+
+:begin
+    set BATNAME=%~nx0
+    echo Старт %BATNAME% ...
+
+    set SCRIPTS_DIR=D:\PROJECTS_LYR\CHECK_LIST\03_SCRIPT\04_BAT\PROJECTS_BAT\TOOLS_BAT
+    set LIB_BAT=%SCRIPTS_DIR%\LIB
+
+    call :CurrentDir || exit /b 1
+    rem  echo CurrentDir: %CurrentDir%
+
+    rem set PN_CAPTION=Ввод значения
+    set P1=P1_default
+    set P1=
+    call :Check_P P1 %1 || exit /b 1
+    rem echo P1: %P1%    
+
+    rem set PN_CAPTION=Ввод значения
+    set P2=P2_default
+    set P2=
+    call :Check_P P2 %2 || exit /b 1
+    rem echo P2: %P2%    
+
+    if "%P1%"=="" (
+        echo ERROR: Параметр P1 (project) не задан...
+        echo Использование: %BATNAME% P1 [P2] [...]
+    ) else (
+        call :MAIN_FUNC new %1 %2 %3 %4 %5 %6 %7 %8 %9
+    )
+
+    rem echo Использование: %BATNAME% [P1] [P2] [...]
+    rem call :MAIN_FUNC %1 %2 %3 %4 %5 %6 %7 %8 %9
+
+:Exit
+exit /b 0
+
+rem --------------------------------------------------------------------------------
+rem procedure MAIN_FUNC ()
+rem --------------------------------------------------------------------------------
+:MAIN_FUNC
+rem beginfunction
+    set FUNCNAME=%0
+    if "%DEBUG%"=="1" (
+        echo DEBUG: procedure %FUNCNAME% ...
+    )
+
+    C:\Users\lyr\AppData\Local\Programs\Python\Python312\Scripts\poetry.exe %1 %2 %3 %4 %5 %6 %7 %8 %9
+
+    exit /b 0
+rem endfunction
+
+rem =================================================
+rem ФУНКЦИИ LIB
+rem =================================================
+:Check_P
+%LIB_BAT%\LYRSupport.bat %*
+exit /b 0
+:ExtractFileDir
+%LIB_BAT%\LYRFileUtils.bat %*
+exit /b 0
+:FullFileName
+%LIB_BAT%\LYRFileUtils.bat %*
+exit /b 0
+:ExtractFileName
+%LIB_BAT%\LYRFileUtils.bat %*
+exit /b 0
+:ExtractFileNameWithoutExt
+%LIB_BAT%\LYRFileUtils.bat %*
+exit /b 0
+:ExtractFileExt
+%LIB_BAT%\LYRFileUtils.bat %*
+exit /b 0
+:FileAttr
+%LIB_BAT%\LYRFileUtils.bat %*
+exit /b 0
+:CurrentDir
+%LIB_BAT%\LYRFileUtils.bat %*
+exit /b 0
+rem =================================================
