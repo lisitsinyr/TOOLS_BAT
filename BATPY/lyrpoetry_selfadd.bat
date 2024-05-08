234c4a41930eb1842d095646cbc880d13f1ae66b
lisitsinyr <lisitsinyr@gmail.com>
2024-05-08 20:42:22 +0400

    20240508204219

---
+++ BATPY/lyrpoetry_selfadd.bat
@@ -0,0 +1,131 @@
+@echo off
+rem -------------------------------------------------------------------
+rem lyrpoetry_selfadd.bat
+rem     «‡ÔÛÒÍ poetry ËÁ „ÎÓ·‡Î¸ÌÓ„Ó ‚ËÚÛ‡Î¸ÌÓ„Ó ÔÓÒÚ‡ÌÒÚ‚‡
+rem Poetry (version 1.8.2)
+rem 
+rem Usage:
+rem   command [options] [arguments]
+rem 
+rem Options:
+rem   -h, --help                 Display help for the given command. When no command is given display help for the list command.
+rem   -q, --quiet                Do not output any message.
+rem   -V, --version              Display this application version.
+rem       --ansi                 Force ANSI output.
+rem       --no-ansi              Disable ANSI output.
+rem   -n, --no-interaction       Do not ask any interactive question.
+rem       --no-plugins           Disables plugins.
+rem       --no-cache             Disables Poetry source caches.
+rem   -C, --directory=DIRECTORY  The working directory for the Poetry command (defaults to the current working directory).
+rem   -v|vv|vvv, --verbose       Increase the verbosity of messages: 1 for normal output, 2 for more verbose output and 3 for debug.
+rem 
+rem -------------------------------------------------------------------
+rem   self add - Add additional packages to Poetry's runtime environment.
+rem              The self add command installs Poetry plugins and make them available at runtime. Additionally, it can also be used to upgrade Poetryís own dependencies or inject additional packages into the runtime environment
+rem -------------------------------------------------------------------
+rem   Usage 
+rem   For example, to install the poetry-plugin-export plugin, you can run:
+rem   poetry self add poetry-plugin-export
+rem   To update to the latest poetry-core version, you can run:
+rem   poetry self add poetry-core@latest
+rem   Options
+rem   --editable (-e): Add vcs/path dependencies as editable.
+rem   --extras (-E): Extras to activate for the dependency. (multiple values allowed)
+rem   --allow-prereleases: Accept prereleases.
+rem   --source: Name of the source to use to install the package.
+rem   --dry-run: Output the operations but do not execute anything (implicitly enables ñverbose).
+rem -------------------------------------------------------------------
+chcp 1251>NUL
+
+setlocal enabledelayedexpansion
+
+:begin
+    set BATNAME=%~nx0
+    echo —Ú‡Ú %BATNAME% ...
+
+    set SCRIPTS_DIR=D:\PROJECTS_LYR\CHECK_LIST\03_SCRIPT\04_BAT\PROJECTS_BAT\TOOLS_BAT
+    set LIB_BAT=%SCRIPTS_DIR%\LIB
+    call :CurrentDir || exit /b 1
+    rem  echo CurrentDir: %CurrentDir%
+
+    echo Publishes a package to a remote repository ...
+    set COMMAND=self add
+    set APPRUN=poetry -v %COMMAND%
+
+    set P1=
+    call :Check_P P1 %1 || exit /b 1
+   
+    if "%P1%"=="" (
+        rem call :MAIN_FUNC
+        set APPRUN=poetry %*
+    ) else (
+        set APPRUN=poetry %*
+    )
+    echo APPRUN: %APPRUN%
+    %APPRUN%
+
+:Exit
+exit /b 0
+
+rem --------------------------------------------------------------------------------
+rem procedure MAIN_FUNC ()
+rem --------------------------------------------------------------------------------
+:MAIN_FUNC
+rem beginfunction
+    set FUNCNAME=%0
+    if "%DEBUG%"=="1" (
+        echo DEBUG: procedure %FUNCNAME% ...
+    )
+
+    set dry-run=
+    set PN_CAPTION=dry-run
+    call :Read_P dry-run %1 || exit /b 1
+    rem echo dry-run: %dry-run%
+    if not "%dry-run%"=="" (
+        set APPRUN=%APPRUN% --dry-run %dry-run%
+    )
+    set lock=
+    set PN_CAPTION=lock
+    call :Read_P lock %1 || exit /b 1
+    rem echo lock: %lock%
+    if not "%lock%"=="" (
+        set APPRUN=%APPRUN% --lock %lock%
+    )
+    
+:Exit
+exit /b 0
+
+rem =================================================
+rem ‘”Õ ÷»» LIB
+rem =================================================
+:Check_P
+%LIB_BAT%\LYRSupport.bat %*
+exit /b 0
+:Read_P
+%LIB_BAT%\LYRSupport.bat %*
+exit /b 0
+:PressAnyKey
+%LIB_BAT%\LYRSupport.bat %*
+exit /b 0
+:ExtractFileDir
+%LIB_BAT%\LYRFileUtils.bat %*
+exit /b 0
+:FullFileName
+%LIB_BAT%\LYRFileUtils.bat %*
+exit /b 0
+:ExtractFileName
+%LIB_BAT%\LYRFileUtils.bat %*
+exit /b 0
+:ExtractFileNameWithoutExt
+%LIB_BAT%\LYRFileUtils.bat %*
+exit /b 0
+:ExtractFileExt
+%LIB_BAT%\LYRFileUtils.bat %*
+exit /b 0
+:FileAttr
+%LIB_BAT%\LYRFileUtils.bat %*
+exit /b 0
+:CurrentDir
+%LIB_BAT%\LYRFileUtils.bat %*
+exit /b 0
+rem =================================================
---
+++ BATPY/lyrpoetry_sourceremove.bat
@@ -0,0 +1,124 @@
+@echo off
+rem -------------------------------------------------------------------
+rem lyrpoetry_sourceremove.bat
+rem     Запуск poetry из глобального виртуального пространства
+rem Poetry (version 1.8.2)
+rem 
+rem Usage:
+rem   command [options] [arguments]
+rem 
+rem Options:
+rem   -h, --help                 Display help for the given command. When no command is given display help for the list command.
+rem   -q, --quiet                Do not output any message.
+rem   -V, --version              Display this application version.
+rem       --ansi                 Force ANSI output.
+rem       --no-ansi              Disable ANSI output.
+rem   -n, --no-interaction       Do not ask any interactive question.
+rem       --no-plugins           Disables plugins.
+rem       --no-cache             Disables Poetry source caches.
+rem   -C, --directory=DIRECTORY  The working directory for the Poetry command (defaults to the current working directory).
+rem   -v|vv|vvv, --verbose       Increase the verbosity of messages: 1 for normal output, 2 for more verbose output and 3 for debug.
+rem 
+rem -------------------------------------------------------------------
+rem   source remove - Remove source configured for the project.
+rem                The source remove command removes a configured source from your pyproject.toml.
+rem -------------------------------------------------------------------
+rem   Usage 
+rem   The source remove command removes a configured source from your pyproject.toml.
+rem   poetry source remove pypi-test
+rem   Options
+rem -------------------------------------------------------------------
+chcp 1251>NUL
+
+setlocal enabledelayedexpansion
+
+:begin
+    set BATNAME=%~nx0
+    echo Старт %BATNAME% ...
+
+    set SCRIPTS_DIR=D:\PROJECTS_LYR\CHECK_LIST\03_SCRIPT\04_BAT\PROJECTS_BAT\TOOLS_BAT
+    set LIB_BAT=%SCRIPTS_DIR%\LIB
+    call :CurrentDir || exit /b 1
+    rem  echo CurrentDir: %CurrentDir%
+
+    echo Publishes a package to a remote repository ...
+    set COMMAND=source remove
+    set APPRUN=poetry -v %COMMAND%
+
+    set P1=
+    call :Check_P P1 %1 || exit /b 1
+   
+    if "%P1%"=="" (
+        rem call :MAIN_FUNC
+        set APPRUN=poetry %*
+    ) else (
+        set APPRUN=poetry %*
+    )
+    echo APPRUN: %APPRUN%
+    %APPRUN%
+
+:Exit
+exit /b 0
+
+rem --------------------------------------------------------------------------------
+rem procedure MAIN_FUNC ()
+rem --------------------------------------------------------------------------------
+:MAIN_FUNC
+rem beginfunction
+    set FUNCNAME=%0
+    if "%DEBUG%"=="1" (
+        echo DEBUG: procedure %FUNCNAME% ...
+    )
+
+    set dry-run=
+    set PN_CAPTION=dry-run
+    call :Read_P dry-run %1 || exit /b 1
+    rem echo dry-run: %dry-run%
+    if not "%dry-run%"=="" (
+        set APPRUN=%APPRUN% --dry-run %dry-run%
+    )
+    set lock=
+    set PN_CAPTION=lock
+    call :Read_P lock %1 || exit /b 1
+    rem echo lock: %lock%
+    if not "%lock%"=="" (
+        set APPRUN=%APPRUN% --lock %lock%
+    )
+    
+:Exit
+exit /b 0
+
+rem =================================================
+rem ФУНКЦИИ LIB
+rem =================================================
+:Check_P
+%LIB_BAT%\LYRSupport.bat %*
+exit /b 0
+:Read_P
+%LIB_BAT%\LYRSupport.bat %*
+exit /b 0
+:PressAnyKey
+%LIB_BAT%\LYRSupport.bat %*
+exit /b 0
+:ExtractFileDir
+%LIB_BAT%\LYRFileUtils.bat %*
+exit /b 0
+:FullFileName
+%LIB_BAT%\LYRFileUtils.bat %*
+exit /b 0
+:ExtractFileName
+%LIB_BAT%\LYRFileUtils.bat %*
+exit /b 0
+:ExtractFileNameWithoutExt
+%LIB_BAT%\LYRFileUtils.bat %*
+exit /b 0
+:ExtractFileExt
+%LIB_BAT%\LYRFileUtils.bat %*
+exit /b 0
+:FileAttr
+%LIB_BAT%\LYRFileUtils.bat %*
+exit /b 0
+:CurrentDir
+%LIB_BAT%\LYRFileUtils.bat %*
+exit /b 0
+rem =================================================
