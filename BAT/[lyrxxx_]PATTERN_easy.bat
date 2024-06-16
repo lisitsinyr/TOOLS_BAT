90539be7275acde93db5e8314541c8113114dd2b
lisitsinyr <lisitsinyr@gmail.com>
2024-06-16 08:32:56 +0400

    20240616 83255

---
+++ BAT/[lyrxxx_]PATTERN_easy.bat
@@ -0,0 +1,153 @@
+@echo off
+rem -------------------------------------------------------------------
+rem [lyrxxx_]PATTERN_easy.bat
+rem -------------------------------------------------------------------
+chcp 1251>NUL
+
+setlocal enabledelayedexpansion
+
+rem --------------------------------------------------------------------------------
+rem 
+rem --------------------------------------------------------------------------------
+:begin
+    call :MAIN %* || exit /b 1
+
+    exit /b 0
+:end
+rem --------------------------------------------------------------------------------
+
+rem -----------------------------------------------
+rem procedure MAIN_INIT ()
+rem -----------------------------------------------
+:MAIN_INIT
+rem beginfunction
+    set FUNCNAME=%0
+    set FUNCNAME=MAIN_INIT
+    if defined DEBUG (
+        echo DEBUG: procedure !FUNCNAME! ...
+    )
+    rem -------------------------------------------------------------------
+    rem SCRIPTS_DIR - Каталог скриптов
+    rem -------------------------------------------------------------------
+    if not defined SCRIPTS_DIR (
+        set SCRIPTS_DIR=D:\PROJECTS_LYR\CHECK_LIST\03_SCRIPT\04_BAT\PROJECTS_BAT\TOOLS_SRC_BAT
+    )
+
+    rem -------------------------------------------------------------------
+    rem LIB_BAT - каталог библиотеки скриптов
+    rem -------------------------------------------------------------------
+    set LIB_BAT=!SCRIPTS_DIR!\LIB
+
+    rem -------------------------------------------------------------------
+    rem SCRIPTS_DIR_KIX - Каталог скриптов KIX
+    rem -------------------------------------------------------------------
+    if not defined SCRIPTS_DIR_KIX (
+        set SCRIPTS_DIR_KIX=D:\PROJECTS_LYR\CHECK_LIST\03_SCRIPT\01_KIX\PROJECTS_KIX\TOOLS_SRC_KIX
+    )
+
+    exit /b 0
+rem endfunction
+
+rem =================================================
+rem procedure MAIN (%*)
+rem =================================================
+:MAIN
+rem beginfunction
+    set FUNCNAME=%0
+    set FUNCNAME=MAIN
+    if defined DEBUG (
+        echo DEBUG: procedure !FUNCNAME! ...
+    )
+
+    set BATNAME=%~nx0
+    echo Start !BATNAME! ...
+
+    set DEBUG=
+    set /a LOG_FILE_ADD=0
+
+    call :MAIN_INIT || exit /b 1
+
+    rem Количество аргументов
+    call :Read_N %* || exit /b 1
+
+    call :SET_LIB %~f0 || exit /b 1
+
+    rem -------------------------------------
+    rem OPTION
+    rem -------------------------------------
+    set O1=O1
+    set PN_CAPTION=O1
+    call :Read_P O1 O1 || exit /b 1
+    rem echo O1:!O1!
+    if defined O1 (
+        set OPTION=!OPTION! --O1 !O1!
+    ) else (
+        echo INFO: O1 not defined ...
+    )
+
+    rem -------------------------------------
+    rem ARGS
+    rem -------------------------------------
+    rem Проверка на обязательные аргументы
+    set A1=
+    set PN_CAPTION=A1
+    call :Read_P A1 A1 || exit /b 1
+    rem echo A1:!A1!
+    if defined A1 (
+        set ARGS=!ARGS! !A1!
+    ) else (
+        echo ERROR: A1 not defined ...
+        set OK=
+    )
+
+    rem set APP_KIX_DIR=D:\PROJECTS_LYR\CHECK_LIST\03_SCRIPT\01_KIX\PROJECTS_KIX\TOOLS_KIX\KIX
+    rem set APP_KIX=
+
+    rem call :SET_KIX || exit /b 1
+
+    rem if exist !APP_KIX_DIR!\!APP_KIX!.kix (
+    rem     echo START !APP_KIX_DIR!\!APP_KIX!.kix ... 
+    rem     kix32.exe !APP_KIX_DIR!\!APP_KIX!.kix "$A1=!A1!"
+    rem )
+
+    rem call :PressAnyKey || exit /b 1
+
+    exit /b 0
+:end
+rem --------------------------------------------------------------------------------
+
+rem =================================================
+rem ФУНКЦИИ LIB
+rem =================================================
+
+rem =================================================
+rem LYRConst.bat
+rem =================================================
+:SET_LIB
+%LIB_BAT%\LYRConst.bat %*
+exit /b 0
+:SET_KIX
+%LIB_BAT%\LYRConst.bat %*
+exit /b 0
+rem =================================================
+rem LYRDateTime.bat
+rem =================================================
+rem =================================================
+rem LYRFileUtils.bat
+rem =================================================
+rem =================================================
+rem LYRLog.bat
+rem =================================================
+rem =================================================
+rem LYRStrUtils.bat
+rem =================================================
+rem =================================================
+rem LYRSupport.bat
+rem =================================================
+:Read_N
+%LIB_BAT%\LYRSupport.bat %*
+exit /b 0
+:Read_P
+%LIB_BAT%\LYRSupport.bat %*
+exit /b 0
+rem =================================================
