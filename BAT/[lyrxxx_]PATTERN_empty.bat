bb10d444e4c046dfc9c48aff5dcad5e2607224e3
lisitsinyr <lisitsinyr@gmail.com>
2025-10-26 09:47:43 +0400

    20251026

--- BAT/[lyrxxx_]PATTERN_empty.bat
+++ BAT/[lyrxxx_]PATTERN_empty.bat
@@ -13,6 +13,54 @@ setlocal enabledelayedexpansion
     set GERRORLEVEL=!errorlevel!
     echo GERRORLEVEL:!GERRORLEVEL!
 
+    rem ---------------------------------------------
+    rem IF
+    rem ---------------------------------------------
+    if (condition_01) (
+        ...
+    ) else (
+        ...
+    )
+
+    rem ---------------------------------------------
+    rem IF AND
+    rem ---------------------------------------------
+    if (condition_01) (
+        if (condition_02) (
+            echo AND ...
+        )
+    )
+
+    rem ---------------------------------------------
+    rem IF OR
+    rem ---------------------------------------------
+    set result=F
+    if (condition_01) set result=T
+    if (condition_02) set result=T
+    if "!result!"=="T" (
+        echo OR ...
+    )    
+
+    rem ---------------------------------------------
+    rem CASE
+    rem ---------------------------------------------
+    if (condition_01) (
+        ...
+    )
+    if (condition_02) (
+        ...
+    )
+
+    rem ---------------------------------------------
+    rem WHILE
+    rem ---------------------------------------------
+    set Result=
+    :WHILE
+    if not !Result!==10 (
+        ...
+        goto :WHILE
+    )
+
     exit /b 0
 :end
 
